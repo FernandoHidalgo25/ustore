a327d84eb7dbac87edc2b9d2a8eea81a8cd9f909
github username <github email address>
2021-12-27 02:17:55 -0500

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,21 @@ import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { logout } from '../redux/actionCreators'
 
-function Nav({logout}){
-    return <nav>
+function Nav({logout, username}){
+
+    const loggedInRender = () => 
+    <nav>
         <NavLink to="/stores"><button>See all Stores!!</button></NavLink>
         <br/>
-        <button onClick={logout}>Logout!</button> 
+        <button onClick={logout}>Logout!</button>
     </nav>
+
+    const loggedOutRender = () =>
+    <nav>Hello, user please sign in or sign up!</nav>
+
+    return username ? loggedInRender() : loggedOutRender()
 }
 
-export default connect(null, {logout})(Nav);
+const mapStateToProps = (state) => ({username: state.user.username})
+
+export default connect(mapStateToProps, {logout})(Nav);
