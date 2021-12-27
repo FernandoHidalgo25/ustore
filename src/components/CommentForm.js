1ad86f7467e18f1737bde36048ec58298d14cfe7
github username <github email address>
2021-12-27 04:40:53 -0500

    update comment

---
+++ src/components/CommentForm.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import { connect } from 'react-redux'
+//import { submitComment } from '../redux/actionCreators'
+
+function CommentForm({storeId}){
+
+    const [rating, setRating] = useState(5)
+    const [content, setContent] = useState("")
+
+    const onSubmit = (e) => {
+        e.preventDefault() 
+        const newComment = {rating, content, storeId}
+        console.log(newComment)
+    }
+
+    return <form className="new_comment" onSubmit={onSubmit}>
+        <label>
+            Rating:
+            <input type="number" name="rating" min="1" max="5" onChange={(e) => setRating(e.target.value)} value={rating}/>
+        </label><br/>
+        <label>
+            Comment:<br/>
+            <textarea name="content" onChange={(e) => setContent(e.target.value)} value={content}></textarea>
+        </label>
+        <input type="submit" value="Submit" />
+    </form>
+}
+
+const mapStateToProps = (state) => ({storeId: state.selectedStore.id})
+
+export default connect(mapStateToProps)(CommentForm)
+//export default connect(mapStateToProps, {submitComment})(CommentForm)
--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,5 @@ export { default as StoreIndex } from '../containers/StoreIndex';
 export { default as Auth } from './Auth';
 export { default as Nav } from './Nav';
 export { default as CommentCard } from './CommentCard';
-//export { default as CommentForm } from './CommentForm';
+export { default as CommentForm } from './CommentForm';
 export { default as Comments } from '../containers/Comments';
